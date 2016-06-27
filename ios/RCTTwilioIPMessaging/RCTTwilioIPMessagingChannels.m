ca4fd885b2ce8a28e8d7c7d28d1c8dbc4d18e0cf
Brad Bumbalough <bbumbalough@tccm.org>
2016-06-27 16:17:05 -0400

    Removed memberWithIdentity and members as they should be called from RCTTwilioIPMessagingMembers.m. Updated method names to match universal APIs.

--- ios/RCTTwilioIPMessaging/RCTTwilioIPMessagingChannels.m
+++ ios/RCTTwilioIPMessaging/RCTTwilioIPMessagingChannels.m
@@ -22,7 +22,7 @@ RCT_EXPORT_MODULE();
 
 #pragma mark Channel Methods
 
-RCT_REMAP_METHOD(allObjects, allObjects_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getChannels, allObjects_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TwilioIPMessagingClient *client = [[RCTTwilioIPMessagingClient sharedManager] client];
   NSArray<TWMChannel *> *channels = [[client channelsList] allObjects];
   NSMutableArray *response = [NSMutableArray array];
@@ -34,7 +34,7 @@ RCT_REMAP_METHOD(allObjects, allObjects_resolver:(RCTPromiseResolveBlock)resolve
   resolve(RCTNullIfNil(response));
 }
 
-RCT_REMAP_METHOD(createChannelWithOptions, options:(NSDictionary *)options create_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(createChannel, options:(NSDictionary *)options create_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TwilioIPMessagingClient *client = [[RCTTwilioIPMessagingClient sharedManager] client];
   [[client channelsList] createChannelWithOptions:options completion:^(TWMResult *result, TWMChannel *channel) {
     if (result.isSuccessful) {
@@ -46,7 +46,7 @@ RCT_REMAP_METHOD(createChannelWithOptions, options:(NSDictionary *)options creat
   }];
 }
 
-RCT_REMAP_METHOD(channelWithId, sid:(NSString *)sid sid_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getChannel, sid:(NSString *)sid sid_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TwilioIPMessagingClient *client = [[RCTTwilioIPMessagingClient sharedManager] client];
   TWMChannel *channel = [[client channelsList] channelWithId:sid];
   if (channel) {
@@ -57,7 +57,7 @@ RCT_REMAP_METHOD(channelWithId, sid:(NSString *)sid sid_resolver:(RCTPromiseReso
   }
 }
 
-RCT_REMAP_METHOD(channelWithUniqueName, uniqueName:(NSString *)uniqueName sid_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getChannelByUniqueName, uniqueName:(NSString *)uniqueName sid_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TwilioIPMessagingClient *client = [[RCTTwilioIPMessagingClient sharedManager] client];
   TWMChannel *channel = [[client channelsList] channelWithUniqueName:uniqueName];
   if (channel) {
@@ -70,12 +70,7 @@ RCT_REMAP_METHOD(channelWithUniqueName, uniqueName:(NSString *)uniqueName sid_re
 
 #pragma mark Channel Instance Methods
 
-RCT_REMAP_METHOD(members, sid:(NSString *)sid members_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  TWMChannel *channel = [self loadChannelFromSid:sid];
-  resolve([RCTConvert TWMMembers:channel.members.allObjects]);
-}
-
-RCT_REMAP_METHOD(synchronizeWithCompletion, sid:(NSString *)sid synchronize_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(synchronize, sid:(NSString *)sid synchronize_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel synchronizeWithCompletion:^(TWMResult *result) {
     if (result.isSuccessful) {
@@ -123,7 +118,7 @@ RCT_REMAP_METHOD(setUniqueName, sid:(NSString *)sid uniqueName:(NSString *)uniqu
   }];
 }
 
-RCT_REMAP_METHOD(joinWithCompletion, sid:(NSString *)sid join_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(join, sid:(NSString *)sid join_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel joinWithCompletion:^(TWMResult *result) {
     if (result.isSuccessful) {
@@ -135,7 +130,7 @@ RCT_REMAP_METHOD(joinWithCompletion, sid:(NSString *)sid join_resolver:(RCTPromi
   }];
 }
 
-RCT_REMAP_METHOD(declineInvitationWithCompletion, sid:(NSString *)sid decline_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(declineInvitation, sid:(NSString *)sid decline_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel declineInvitationWithCompletion:^(TWMResult *result) {
     if (result.isSuccessful) {
@@ -147,7 +142,7 @@ RCT_REMAP_METHOD(declineInvitationWithCompletion, sid:(NSString *)sid decline_re
   }];
 }
 
-RCT_REMAP_METHOD(leaveWithCompletion, sid:(NSString *)sid leave_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(leave, sid:(NSString *)sid leave_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel leaveWithCompletion:^(TWMResult *result) {
     if (result.isSuccessful) {
@@ -159,7 +154,7 @@ RCT_REMAP_METHOD(leaveWithCompletion, sid:(NSString *)sid leave_resolver:(RCTPro
   }];
 }
 
-RCT_REMAP_METHOD(destroyWithCompletion, sid:(NSString *)sid destroy_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(destroy, sid:(NSString *)sid destroy_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel destroyWithCompletion:^(TWMResult *result) {
     if (result.isSuccessful) {
@@ -175,11 +170,6 @@ RCT_REMAP_METHOD(destroyWithCompletion, sid:(NSString *)sid destroy_resolver:(RC
 RCT_REMAP_METHOD(typing, sid:(NSString *)sid) {
   TWMChannel *channel = [self loadChannelFromSid:sid];
   [channel typing];
-}
-
-RCT_REMAP_METHOD(memberWithIdentity, sid:(NSString *)sid identity:(NSString *)identity member_resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  TWMChannel *channel = [self loadChannelFromSid:sid];
-  resolve([RCTConvert TWMMember:[channel memberWithIdentity:identity]]);
 }
 
 @end
