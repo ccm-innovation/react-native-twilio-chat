f8d0b4a05c4b8e7f9635420c7fd2dd088352a449
Benoist Claassen <benoist.claassen@gmail.com>
2017-05-16 11:28:39 +0200

    added test setup

--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -2,6 +2,7 @@ package com.example;
 
 import android.app.Application;
 
+import com.bradbumbalough.RCTTwilioChat.RCTTwilioChatPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+              new RCTTwilioChatPackage()
       );
     }
   };
